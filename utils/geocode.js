6f39ef8c379fadd575089cecf51835a4ee7e5229
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2021-06-02 08:37:44 +0530

    Updating api key for map_box

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,9 +1,9 @@
 const request = require('postman-request')
 
-const mapboxkey = "pk.eyJ1IjoicHV0dGEiLCJhIjoiY2twYXh1OXp3MDVuazJ2cnJ4eTV1Yjg4MiJ9.oZnG5N9wDxbH1eAa2ZSTMw"
+const mbox = process.env.MAPBOX_PUBLIC
 
 const getGeocode = (location, callback)=>{
-    let mapbox_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${mapboxkey}&limit=1`;
+    let mapbox_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${mbox}&limit=1`;
     request({url: mapbox_url, json: true}, (err, res, body)=>{
         try {
             if(err){
